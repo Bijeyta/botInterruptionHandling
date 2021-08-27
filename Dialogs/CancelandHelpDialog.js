6268f4ec885886ec1796e201ddcfd2714df647d4
Bijeyta Tiwary <bijeyta.tiwary@celebaltech.com>
2021-08-27 17:38:20 +0530

    second commit

--- Dialogs/CancelandHelpDialog.js
+++ Dialogs/CancelandHelpDialog.js
@@ -1,10 +1,47 @@
-const { InputHints } = require('botbuilder');
-const { ComponentDialog, DialogTurnStatus } = require('botbuilder-dialogs');
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
-class CancelandHelpDialog extends ComponentDialog {
+const { InputHints,MessageFactory } = require('botbuilder');
+const { ComponentDialog,ConfirmPrompt,ChoicePrompt,ChoiceFactory, DialogTurnStatus } = require('botbuilder-dialogs');
+//const { DateResolverDialog } = require('./dateResolverDialog');
+const DATE_RESOLVER_DIALOG = 'dateResolverDialog';
+//const ChoicePromptDialog = 'choicePromptDialog';
+const CONFIRM_PROMPT = 'confirmPrompt';
+
+
+
+/**
+ * This base class watches for common phrases like "help" and "cancel" and takes action on them
+ * BEFORE they reach the normal bot logic.
+ */
+class CancelAndHelpDialog extends ComponentDialog {
+    
+    
+    
     async onContinueDialog(innerDc) {
+      //  this.addDialog(new ConfirmPrompt(CONFIRM_PROMPT))
         const result = await this.interrupt(innerDc);
+        console.log("hii",innerDc.context.activity.text)
+        if (innerDc.context.activity.text=="No") { 
+            console.log("hhhhhh")
+         return await innerDc.cancelAllDialogs() }
+       // console.log(innerDc,"result")
         if (result) {
+           
+
+            // if(innerDc.context.activity.text=="No"){
+
+            // }
+            // else {
+                
+            // }
+            
+            // await innerDc.prompt(CONFIRM_PROMPT);
+        //     const messageText = "do you want to resume your previous booking"
+        //     const msg = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
+        //    await innerDc.prompt(CONFIRM_PROMPT, { prompt: msg });
+
+            // await innerDc.replaceDialog(DATE_RESOLVER_DIALOG);
             return result;
         }
         return await super.onContinueDialog(innerDc);
@@ -12,24 +49,60 @@ class CancelandHelpDialog extends ComponentDialog {
 
     async interrupt(innerDc) {
         if (innerDc.context.activity.text) {
+          // this.addDialog(new ConfirmPrompt(ChoicePromptDialog))
             const text = innerDc.context.activity.text.toLowerCase();
-            
-            switch(text) {
-                case 'help':
-                case '?': {
-                    const helpMessageText = 'Show help here';
-                    await innerDc.context.sendActivity(helpMessageText, InputHints.ExpectingInput);
+
+            // switch (text) {
+            // case 'help':
+            // case '?': {
+            //     const helpMessageText = 'Show help here';
+            //     await innerDc.context.sendActivity(helpMessageText, helpMessageText, InputHints.ExpectingInput);
+            //     return { status: DialogTurnStatus.waiting };
+            // }
+            // case 'cancel':
+            // case 'quit': {
+            //     const cancelMessageText = 'Cancelling...';
+            //     await innerDc.context.sendActivity(cancelMessageText, cancelMessageText, InputHints.IgnoringInput);
+            //     return await innerDc.cancelAllDialogs();
+            // }
+            // }
+           // console.log("kkk",text)
+            switch (text) {
+                
+                
+        
+                case 'payroll': {
+                    
+                    // We haven't implemented the GetWeatherDialog so we just display a TODO message.
+                    // const getWeatherMessageText = 'TODO: get weather flow here';
+                   // const msg = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
+                  // await innerDc.prompt(CONFIRM_PROMPT, { prompt: "llll" });
+                    // await innerDc.context.sendActivity(getWeatherMessageText);
+                    await innerDc.beginDialog(applyLeaveDialog);
+                    const messageText = "do you want to resume your previous booking"
+                    const msg = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
+                    await innerDc.prompt(CONFIRM_PROMPT, { prompt: msg });
+                  // await innerDc.prompt(CONFIRM_PROMPT, { prompt: "llll" });
+
+                    //  await innerDc.context.prompt(ChoicePromptDialog,{
+                    //     prompt: "Do you want to place your easy order, most recent order, or new order?",
+                    //     choices: ChoiceFactory.toChoices(['Easy Order','Most Recent Order','New Order'])
+                    //   })
+                   //  await innerDc.context.prompt(ChoicePromptDialog, { prompt: "j" });
+                  //  await innerDc.context.sendActivity(getWeatherMessageText, getWeatherMessageText, InputHints.ExpectingInput);
                     return { status: DialogTurnStatus.waiting };
                 }
-                case 'cancel':
-                case 'quit': {
-                    const cancelMessageText = 'Calling...';
-                    await innerDc.context.sendActivity(cancelMessageText, cancelMessageText, InputHints.IgnoringInput);
-                    return await innerDc.cancelAllDialogs();
+                case 'apply leave':
+                    await innerDc.beginDialog(applyLeaveDialog);
+                    const messageText = "do you want to resume your previous booking"
+                    const msg = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
+                    await innerDc.prompt(CONFIRM_PROMPT, { prompt: msg });
+                    return { status: DialogTurnStatus.waiting };
+        
+                
                 }
-            }
         }
     }
 }
 
-module.exports.CancelandHelpDialog = CancelandHelpDialog;
+module.exports.CancelAndHelpDialog = CancelAndHelpDialog;
--- Dialogs/RootDialog.js
+++ Dialogs/RootDialog.js
@@ -1,9 +1,9 @@
 const { ComponentDialog, DialogSet, DialogTurnStatus, WaterfallDialog, runDialog } = require('botbuilder-dialogs');
+const { InputHints, MessageFactory } = require('botbuilder')
 const { rootDialog, helpDialog, applyLeaveDialog, payrollDialog } = require('../Constants/DialogIds');
 const { HelpDialog } = require('./helpDialog');
 const { ApplyLeaveDialog } = require('./applyLeave')
 const { PayrollDialog } = require('./payrollDialog')
-
 const parseMessage = 'parseMessage';
 
 class RootDialog extends ComponentDialog {
@@ -40,48 +40,139 @@ class RootDialog extends ComponentDialog {
         }
     }
 
-    async onContinueDialog(innerDc) {
-        const result = await this.interrupt(innerDc);
-        if (result) {
-            return result;
-        }
-        return await super.onContinueDialog(innerDc);
-    }
+    // async onContinueDialog(innerDc) {
+    //     const result = await this.interrupt(innerDc);
+    //     if (result) {
+    //         return result;
+    //     }
+    //     return await super.onContinueDialog(innerDc);
+    // }
 
-    async interrupt(innerDc) {
-        if (innerDc.context.activity.text) {
-            const text = innerDc.context.activity.text.trim().toLowerCase();
-            let interruptlist = ['help', '?', 'exit', 'cancel', 'quit', 'bye', 'apply leave', 'payroll'];
-            if (interruptlist.includes(text)) {
-                console.log('interrupt found');
-                switch (text) {
-                    case 'help':
-                    case '?': { 
+    // async interrupt(innerDc) {
+    //     if (innerDc.context.activity.text) {
+    //         const text = innerDc.context.activity.text.trim().toLowerCase();
+    //         let interruptlist = ['help', '?', 'exit', 'cancel', 'quit', 'bye', 'apply leave', 'payroll'];
+    //         if (interruptlist.includes(text)) {
+    //             console.log('interrupt found');
+    //             switch (text) {
+    //                 case 'help':
+    //                 case '?': { 
                         
-                        await innerDc.context.sendActivity('helpDialog');
-                        return await innerDc.endDialog();
-                    }
-                    case 'exit':
-                    case 'bye':
-                    case 'cancel':
-                    case 'quit': {
-                        await innerDc.context.sendActivity('applyLeave');
-                        return await innerDc.cancelAllDialogs();
-                    }
-                    case 'apply leave':
-                        await  innerDc.cancelAllDialogs();
-                        return await innerDc.beginDialog(applyLeaveDialog);
-                    case 'payroll':
-                        return await innerDc.beginDialog(payrollDialog);
+    //                     await innerDc.context.sendActivity('helpDialog');
+    //                     return await innerDc.endDialog();
+    //                 }
+    //                 case 'exit':
+    //                 case 'bye':
+    //                 case 'cancel':
+    //                 case 'quit': {
+    //                     await innerDc.context.sendActivity('applyLeave');
+    //                     return await innerDc.cancelAllDialogs();
+    //                 }
+    //                 case 'apply leave':
+    //                     // await  innerDc.cancelAllDialogs();
+    //                     await innerDc.beginDialog(applyLeaveDialog);
+    //                     const messageText = "do you want to resume your previous booking"
+    //                     const msg = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
+    //                     await innerDc.prompt(CONFIRM_PROMPT, { prompt: msg });
+    //                     return { status: DialogTurnStatus.waiting };
+    //                 case 'payroll':
+    //                     return await innerDc.beginDialog(payrollDialog);
                     
-                }
-            } else {
+    //             }
+    //         } else {
 
-                return null;
-            }
+    //             return null;
+    //         }
 
-        }
-    }
+    //     }
+    // }
+
+    async onContinueDialog(innerDc) {
+        //  this.addDialog(new ConfirmPrompt(CONFIRM_PROMPT))
+          const result = await this.interrupt(innerDc);
+          console.log("hii",innerDc.context.activity.text)
+          if (innerDc.context.activity.text=="No") { 
+              console.log("hhhhhh")
+           return await innerDc.cancelAllDialogs() }
+         // console.log(innerDc,"result")
+          if (result) {
+             
+  
+              // if(innerDc.context.activity.text=="No"){
+  
+              // }
+              // else {
+                  
+              // }
+              
+              // await innerDc.prompt(CONFIRM_PROMPT);
+          //     const messageText = "do you want to resume your previous booking"
+          //     const msg = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
+          //    await innerDc.prompt(CONFIRM_PROMPT, { prompt: msg });
+  
+              // await innerDc.replaceDialog(DATE_RESOLVER_DIALOG);
+              return result;
+          }
+          return await super.onContinueDialog(innerDc);
+      }
+  
+      async interrupt(innerDc) {
+          if (innerDc.context.activity.text) {
+            // this.addDialog(new ConfirmPrompt(ChoicePromptDialog))
+              const text = innerDc.context.activity.text.toLowerCase();
+  
+              // switch (text) {
+              // case 'help':
+              // case '?': {
+              //     const helpMessageText = 'Show help here';
+              //     await innerDc.context.sendActivity(helpMessageText, helpMessageText, InputHints.ExpectingInput);
+              //     return { status: DialogTurnStatus.waiting };
+              // }
+              // case 'cancel':
+              // case 'quit': {
+              //     const cancelMessageText = 'Cancelling...';
+              //     await innerDc.context.sendActivity(cancelMessageText, cancelMessageText, InputHints.IgnoringInput);
+              //     return await innerDc.cancelAllDialogs();
+              // }
+              // }
+             // console.log("kkk",text)
+              switch (text) {
+                  
+                  
+          
+                  case 'payroll': {
+                      
+                      // We haven't implemented the GetWeatherDialog so we just display a TODO message.
+                      const getWeatherMessageText = 'TODO: get weather flow here';
+                     // const msg = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
+                    // await innerDc.prompt(CONFIRM_PROMPT, { prompt: "llll" });
+                      await innerDc.context.sendActivity(getWeatherMessageText);
+                    //   await innerDc.beginDialog(payrollDialog);
+                      const messageText = "do you want to resume your previous booking"
+                      console.log(messageText);
+                      const msg = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
+                      await innerDc.prompt(CONFIRM_PROMPT, { prompt: msg });
+                    // await innerDc.prompt(CONFIRM_PROMPT, { prompt: "llll" });
+  
+                      //  await innerDc.context.prompt(ChoicePromptDialog,{
+                      //     prompt: "Do you want to place your easy order, most recent order, or new order?",
+                      //     choices: ChoiceFactory.toChoices(['Easy Order','Most Recent Order','New Order'])
+                      //   })
+                     //  await innerDc.context.prompt(ChoicePromptDialog, { prompt: "j" });
+                    //  await innerDc.context.sendActivity(getWeatherMessageText, getWeatherMessageText, InputHints.ExpectingInput);
+                      return { status: DialogTurnStatus.waiting };
+                  }
+                  case 'apply leave':
+                      await innerDc.beginDialog(applyLeaveDialog);
+                      const messageText = "do you want to resume your previous booking"
+                      const msg = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
+                      await innerDc.prompt(CONFIRM_PROMPT, { prompt: msg });
+                      return { status: DialogTurnStatus.waiting };
+          
+                  
+                  }
+          }
+      }
 
     async routeMessage(stepContext) {
         // await stepContext.context.sendActivity('in Route Messages');
